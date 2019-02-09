5583107d9b5ed5ca2f3faa2f2fc44773c93d4d73
falcon2wings <47298134+falcon2wings@users.noreply.github.com>
2019-02-09 21:02:41 +0900

    change USER_SERVICE_UUID for obniz

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '52960b20-4618-4eb2-b243-be1672d527cf'; // LED, Button
+const USER_SERVICE_UUID         = '453d97f8-fd58-433c-8379-55ee42b470d4'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
